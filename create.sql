9347bcb26cd776f0a78e3d5b7285b4239851f782
Chelsea Lewis <istack03@gmail.com>
2012-02-27 21:05:36 -0500

    Updated create.sql

--- create.sql
+++ create.sql
@@ -4,41 +4,40 @@ USE budget;
 
 
 
---
---Table structure for table  transactions  
---
 
-DROP TABLE IF EXISTS  transactions ;
- CREATE TABLE  transactions  (
-	 id  INT(100) NOT NULL AUTO_INCREMENT,
-	 name  VARCHAR(20) NOT NULL,
-	 title  VARCHAR(40) NOT NULL,
-	 amount  INT(5) NOT NULL,
-	 flow  VARCHAR(10) NOT NULL,
-	 dateSubmitted  DATE NOT NULL,
-	PRIMARY KEY ( id )
+
+DROP TABLE IF EXISTS  `transactions`;
+ CREATE TABLE  `transactions`  (
+     `id`  INT(100) NOT NULL AUTO_INCREMENT,
+     `name`  VARCHAR(20) NOT NULL,
+     `title`  VARCHAR(40) NOT NULL,
+     `amount`  INT(5) NOT NULL,
+     `flow`  VARCHAR(10) NOT NULL,
+     `dateSubmitted`  DATE NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
 --
---Dumping data for table  transactions 
+-- Dumping data for table `transactions` 
 --
 
-INSERT INTO  transactions  VALUES(1, 'Example', 'Cupid\'s Revenge Bid', 300, 'Paid', '2/15/2012');
+INSERT INTO  `transactions`  VALUES(1, 'Example', 'Cupid\'s Revenge Bid', 300, 'Paid', '2/15/2012');
 
 --
---Table structure for table  users  
+-- Table structure for table `users`  
 --
 
-DROP TABLE IF EXISTS  users ;
-CREATE TABLE  users  (
-	 id  INT(100) NOT NULL AUTO_INCREMENT,
-	 name  VARCHAR(20) NOT NULL,
-	 password  VARCHAR(40) NOT NULL,
-	PRIMARY KEY ( id )
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+     `id` INT(100) NOT NULL AUTO_INCREMENT,
+     `name`  VARCHAR(20) NOT NULL,
+     `password`  VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
 --
---Dumping data for table  users 
+-- Dumping data for table `users` 
 --
 
-INSERT INTO  users  VALUES(1, 'ADMIN', 'MARYMASSACREBUDGET12');
+INSERT INTO  `users`  VALUES(1, 'ADMIN', 'MARYMASSACREBUDGET12');
+
