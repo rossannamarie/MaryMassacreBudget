0ec428788999a5d682b1c6fa8d7b9009ade7f826
Chelsea Lewis <istack03@gmail.com>
2012-02-28 13:45:33 -0500

    Removed date

--- viewAll.php
+++ viewAll.php
@@ -12,7 +12,6 @@ include 'dbconnect.php';
 	<th> For </th>
 	<th> Amount </th>
 	<th> Flow </th>
-	<th> Date Submitted </th>
 	<?php
 		$query = "SELECT * FROM transactions";
 		$result = mysqli_query ($db, $query)
@@ -23,10 +22,9 @@ include 'dbconnect.php';
 			$title = $row['title'];
 			$amount = $row['amount'];
 			$flow = $row['flow'];
-			$date = $row['dateSubmitted'];
 			$ford = $row['because'];
 			
-			echo "<tr><td>$name</td><td>$title</td><td>$ford</td><td>$amount</td><td>$flow</td><td>$date</td></tr>\n";
+			echo "<tr><td>$name</td><td>$title</td><td>$ford</td><td>$amount</td><td>$flow</td></tr>\n";
 		}
 	?>
 	</table>
