a188402143c6426e3f77f9d79989eded3339c579
Chelsea Lewis <istack03@gmail.com>
2012-03-10 09:22:20 -0500

    Updated database

--- create.sql
+++ create.sql
@@ -37,6 +37,8 @@ CREATE TABLE `users` (
 	 `id` INT(100) NOT NULL AUTO_INCREMENT,
 	 `name`  VARCHAR(20) NOT NULL,
 	 `password`  VARCHAR(40) NOT NULL,
+	 `position` VARCHAR(10) NOT NULL,
+	 `picture` VARCHAR(30) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -44,4 +46,29 @@ CREATE TABLE `users` (
 -- Dumping data for table `users` 
 --
 
-INSERT INTO  `users`  VALUES(1, 'ADMIN', 'MARYMASSACREBUDGET12');
+INSERT INTO  `users`  VALUES(1, 'MaryBeth', 'MARYMASSACREBUDGET12', 'Captain', 'link');
+INSERT INTO  `users`  VALUES(2, 'Devon', 'MARYMASSACREBUDGET12', 'Captain', 'link');
+
+
+-- 
+-- Table structure for table `userLogs`
+--
+
+DROP TABLE IF EXISTS `userLogs`
+CREATE TABLE `userLogs` (
+	`id` INT(100) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(20) NOT NULL,
+	`dateSubmitted` DATE NOT NULL,
+	`link` VARCHAR(30) NOT NULL,
+	PRIMARY KEY(`id`)
+);
+
+--
+-- Dumping data for table `userLogs`
+--
+
+INSERT INTO `userLogs` VALUES(1, 'MaryBeth', '2/12/2012', 'link');
+INSERT INTO `userLogs` VALUES(2, 'Devon', '2/12/2012', 'link');
+
+
+
