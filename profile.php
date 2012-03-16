91ca06def08bb659b3fe9e4adaef71c6fcb8ac7b
Chelsea Lewis <istack03@gmail.com>
2012-03-16 13:57:25 -0400

    Trying to set up the profiles

--- profile.php
+++ profile.php
@@ -5,24 +5,44 @@ session_start();
 <HTML>
 <link rel="stylesheet" type="text/css" href="style.css" />
 <div id="view">
-
-<?php
-	include 'template.php';
-?>
-
 <table width = "100%">
 <tr valign = "top">
-<td>
-<center>*name goes here*</center></br>
-<center><img src="default.jpg" width=200 height=auto></center></br>
-<center>*info goes here*</center>
-</td>
-<td>
-<p>
-*user actions go here*</br></br>
-Example:</br>
-<b>User</b> posted a transaction on <i>1/1/2012</i>.</br><font size=2><u>Link to view transaction goes here</u></font></br>
-</td>
+<?php
+	include 'template.php';
+	include 'dbconnect.php';
+?>
+<?php
+	echo "Hello";
+	$query = "SELECT DISTINCT users.name, dateSubmitted, link, position, picture FROM userLogs INNER JOIN users ON name=name";
+	$result = mysqli_query($db, $query) or die ("Error Querying Database");
+	while ($row = mysqli_fetch_array($result)) 
+	{
+		$name = $row['name'];
+		$date = $row['dateSubmitted'];
+		$link = $row['link'];
+		$position = $row['position'];
+		$picture = $row['picture'];
+		echo "<td>";
+		echo "<center>".$name."</center></br>";
+		echo "<center><img src=".$picture." width=100 height=auto></center></br>";
+		echo "<center>$position</center>";
+		echo "</td>";
+		$query ="SELECT users.name, dateSubmitted, link FROM userLogs INNER JOIN users";
+		result = mysqli_query($db, $query) or die ("Error Querying Database");
+		while ($row = mysqli_fetch_array($result)) 
+		{
+			$name = $row['name'];
+			$date = $row['dateSubmitted'];
+			$link = $row['link'];
+			echo "<td>";
+			echo "<p>";
+			echo "<b>$name</b> posted a transaction on <i>$date</i>.</br><font size=2><u>$link</u></font></br>";
+			echo "</td>";
+		}
+	}
+?>
+
+
 
 </div>
 </HTML>
