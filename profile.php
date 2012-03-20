4fbd60117f7e9e9618e5e6eaa74ece943da880ee
Chelsea Lewis <istack03@gmail.com>
2012-03-20 12:47:05 -0400

    Edits

--- profile.php
+++ profile.php
@@ -23,15 +23,18 @@ session_start();
 		echo "<center>".$name."</center></br>";
 		echo "<center><img src=".$picture." width=100 height=auto></center></br>";
 		echo "<center>$position</center>";
-		$query ="SELECT users.name, dateSubmitted, link FROM userLogs INNER JOIN users ON users.name = userLogs.name";
+		$query ="SELECT users.name, userLogs.id, dateSubmitted, link FROM userLogs INNER JOIN users ON users.name = userLogs.name";
 		$result = mysqli_query($db, $query) or die ("Error Querying Database");
 		while ($row = mysqli_fetch_array($result)) 
 		{
+			echo "<form method=get action=viewTransaction.php>";
 			$tname = $row['name'];
 			//if($name == $tname) {
 			$date = $row['dateSubmitted'];
-			$link = $row['link'];
-			echo "<p><b>$name</b> posted a transaction on <i>$date</i>.</br><font size=2><u>$link</u></font></p>";
+			$link = $row['id'];
+			echo "<p><b>$name</b> posted a transaction on <i>$date</i></p>";
+			echo "<input type=\"submit\" value=\"View transaction\" id = ".$link." />";
+			echo "</form>";
 			//}
 		}
 	}
