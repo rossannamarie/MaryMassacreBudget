32eada822866ff4dd305bfca8460ce7142e2eaa9
rossannamarie <rcatahan@mail.umw.edu>
2012-03-20 13:19:18 -0400

    added register file

---
+++ register.php
@@ -0,0 +1,16 @@
+<title>Mary Massacre Budget</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<div id="banner">
+<div id="title"> Mary Massacre Budget</div>
+</div>
+<div id="register">
+<form name="register" method="get" action="registerController.php">
+<p> Username : <input type="text" name="username" />
+<p> Password : <input type="text" name="password" />
+<p> Retype password : <input type="text" name="repass" />
+<br/>
+<br/>
+<input type="submit" value="Register" class="submitButton" />
+</form>
+</div>
+</HTML>
